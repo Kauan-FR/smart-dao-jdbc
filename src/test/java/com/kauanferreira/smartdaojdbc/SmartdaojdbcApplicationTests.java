1d1d03a010cc39eccb8e6d18c59d9ba4e144c70c
Kauan <kauanferreira3011@gmail.com>
2026-02-17 22:40:20 -0300

    Initial project smart-dao-jdbc

---
+++ src/test/java/com/kauanferreira/smartdaojdbc/SmartdaojdbcApplicationTests.java
@@ -0,0 +1,13 @@
+package com.kauanferreira.smartdaojdbc;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SmartdaojdbcApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
